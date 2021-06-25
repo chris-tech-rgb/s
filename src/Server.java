df4a3aa5674dad6b1840f10bcbda7699a616ada6
ult <afgugewufv@gmail.com>
2021-06-25 13:56:05 +0800

    1

--- src/Server.java
+++ src/Server.java
@@ -21,67 +21,65 @@ public class Server {
             while (true) {
 
                 final Socket connection = serverSocket.accept();
-                pool.execute(new Runnable() {
-                    public void run() {
-                        try {
-                            InputStream inputStream = connection.getInputStream();
-                            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-                            String client_mess = null;
-                            while ((client_mess = br.readLine()) != null) {
-                                System.out.println(connection.toString());
-                                JSONParser jsonParser = new JSONParser();
-                                JSONObject rec = (JSONObject) jsonParser.parse(client_mess);
-                                String function = (String) rec.get("function");
-                                JSONObject content = (JSONObject) rec.get("content");
-                                switch (function) {
-                                    case "login": {
-                                        String username = (String) content.get("username");
-                                        String password = (String) content.get("password");
-                                        System.out.println(username + " is trying to login.");
-                                        Connection con = null;
-                                        Class.forName("org.postgresql.Driver");
-                                        con = DriverManager.getConnection("jdbc:postgresql://47.119.141.11:5432/info", "user", "user123456");
-                                        String sql = "SELECT login('" + username + "','" + password + "');";
-                                        Statement st = (Statement) con.createStatement();
-                                        ResultSet rs = st.executeQuery(sql);
-                                        int res = 3;
-                                        while (rs.next()) res = rs.getInt(1);
-                                        switch (res) {
-                                            case 0: {
-                                                System.out.println("registration succeed.");
-                                            }
-                                                break;
-                                            case 1: {
-                                                System.out.println("login succeed");
-                                            }
-                                                break;
-                                            case 2: {
-                                                System.out.println("login failed.");
-                                            }
-                                                break;
+                pool.execute(() -> {
+                    try {
+                        InputStream inputStream = connection.getInputStream();
+                        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                        String client_mess;
+                        while ((client_mess = br.readLine()) != null) {
+                            System.out.println(connection);
+                            JSONParser jsonParser = new JSONParser();
+                            JSONObject rec = (JSONObject) jsonParser.parse(client_mess);
+                            String function = (String) rec.get("function");
+                            JSONObject content = (JSONObject) rec.get("content");
+                            switch (function) {
+                                case "login": {
+                                    String username = (String) content.get("username");
+                                    String password = (String) content.get("password");
+                                    System.out.println(username + " is trying to login.");
+                                    Connection con;
+                                    Class.forName("org.postgresql.Driver");
+                                    con = DriverManager.getConnection("jdbc:postgresql://47.119.141.11:5432/info", "user", "user123456");
+                                    String sql = "SELECT login('" + username + "','" + password + "');";
+                                    Statement st = con.createStatement();
+                                    ResultSet rs = st.executeQuery(sql);
+                                    int res = 3;
+                                    while (rs.next()) res = rs.getInt(1);
+                                    switch (res) {
+                                        case 0: {
+                                            System.out.println("registration succeed.");
                                         }
-                                        con.close();
-                                        JSONObject ret = new JSONObject();
-                                        ret.put("function", "login-rep");
-                                        ret.put("timestamp", java.util.Calendar.getInstance().getTime().toString());
-                                        ret.put("content", res);
-                                        OutputStream outputStream = connection.getOutputStream();
-                                        Writer writer = new OutputStreamWriter(outputStream);
-                                        writer.write(ret.toString());
-                                        writer.flush();
-                                        connection.shutdownOutput();
+                                            break;
+                                        case 1: {
+                                            System.out.println("login succeed");
+                                        }
+                                            break;
+                                        case 2: {
+                                            System.out.println("login failed.");
+                                        }
+                                            break;
                                     }
-                                        break;
+                                    con.close();
+                                    JSONObject ret = new JSONObject();
+                                    ret.put("function", "login-rep");
+                                    ret.put("timestamp", java.util.Calendar.getInstance().getTime().toString());
+                                    ret.put("content", res);
+                                    OutputStream outputStream = connection.getOutputStream();
+                                    Writer writer = new OutputStreamWriter(outputStream);
+                                    writer.write(ret.toString());
+                                    writer.flush();
+                                    connection.shutdownOutput();
                                 }
+                                    break;
                             }
-                        } catch (IOException | ParseException | ClassNotFoundException | SQLException e) {
+                        }
+                    } catch (IOException | ParseException | ClassNotFoundException | SQLException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            connection.close();
+                        } catch (IOException e) {
                             e.printStackTrace();
-                        } finally {
-                            try {
-                                connection.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
                         }
                     }
                 });
